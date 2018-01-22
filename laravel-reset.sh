9fc3acbea7549c3ed27c22824ec147681dbc56c6
Michael Blair <mhkblair@gmail.com>
2018-01-23 09:39:35 +1100

    Added stop on error and comments

--- laravel-reset.sh
+++ laravel-reset.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
+
+# Stop on error
+set -e
+
+# Clear bootstrap cache, vendor and node_modules folders
 rm -rf bootstrap/cache/*
 rm -rf vendor
 rm -rf node_modules
+
+# Install composer packages
 composer install --optimize-autoloader
+
+# Clear laravel route and view caches
 php artisan route:clear
 php artisan view:clear
+
+# Install nodejs packages and run npm
 npm install
 npm run dev
